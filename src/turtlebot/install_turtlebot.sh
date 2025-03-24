589663177f4efa7d40335f36a860b223b9f21375
Carsten Nielsen <cars2109@gmail.com>
2025-03-24 13:18:39 +0100

    install scripts

---
+++ src/turtlebot/install_turtlebot.sh
@@ -0,0 +1,36 @@
+sudo apt install ros-humble-gazebo-*
+
+sudo apt install ros-humble-cartographer
+sudo apt install ros-humble-cartographer-ros
+
+sudo apt install ros-humble-navigation2
+sudo apt install ros-humble-nav2-bringup
+
+
+git clone -b humble https://github.com/ROBOTIS-GIT/DynamixelSDK.git
+git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone -b humble https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+
+sudo apt install python3-colcon-common-extensions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
