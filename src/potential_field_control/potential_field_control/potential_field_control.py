21aef6c296aaad2b4f96ce848d49e185c9a46eb6
Carsten Nielsen <cars2109@gmail.com>
2025-03-25 11:57:16 +0100

    remove for not needed stuff

--- src/potential_field_control/potential_field_control/potential_field_control.py
+++ src/potential_field_control/potential_field_control/potential_field_control.py
@@ -21,17 +21,6 @@ class PotentialFieldController(Node):
         self.control_weight = 0.5  # Adaptive weight
 
     def lidar_callback(self, msg):
-        # Convert LIDAR scan to potential field avoidance command
-        min_distance = min(msg.ranges)
-        angle_index = np.argmin(msg.ranges)
-
-        if min_distance < 0.5:  # If obstacle is within 50 cm
-            self.potential_field_cmd.linear.x = 0.0
-            self.potential_field_cmd.angular.z = np.sign(angle_index - len(msg.ranges)/2) * 0.5  # Turn away
-        else:
-            self.potential_field_cmd.linear.x = 0.2
-            self.potential_field_cmd.angular.z = 0.0
-
         self.compute_final_command()
 
     def user_input_callback(self, msg):
@@ -39,17 +28,6 @@ class PotentialFieldController(Node):
         self.compute_final_command()
 
     def compute_final_command(self):
-        # Adjust control weight based on proximity to obstacles
-        if abs(self.potential_field_cmd.angular.z) > 0:
-            self.control_weight = 1.0  # Full autonomy near obstacles
-        else:
-            self.control_weight = 0.2  # More human control away from obstacles
-
-        # Shared control blend
-        final_cmd = Twist()
-        final_cmd.linear.x = (1 - self.control_weight) * self.user_cmd.linear.x + self.control_weight * self.potential_field_cmd.linear.x
-        final_cmd.angular.z = (1 - self.control_weight) * self.user_cmd.angular.z + self.control_weight * self.potential_field_cmd.angular.z
-
         self.cmd_pub.publish(final_cmd)
 
 def main(args=None):
