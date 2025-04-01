fixed my mistakes in the ks calculation

src/potential_field_control/potential_field_control/potential_field_control.py
@@ -17,18 +17,15 @@ class PotentialFieldController(Node):
 
         # Control Variables
         self.user_cmd = Twist()
-        self.potential_field_cmd = Twist()
-        self.control_weight = 0.5  # Adaptive weight
- 
 
-        self.roj = 0.37  # Radius of obstacle
 
         self.beta = 5.0  # Controls steepness of ks1
         self.gamma = 5.0  # Controls steepness of ks2
         self.epsilon = 0.01  # Small constant to avoid div by 0
+        self.rd = 0.5    # Danger zone threshold
+        self.rh = 0.7    # Hysteresis (safe zone buffer)
 
         self.region_state = "safe"  # Track robot zone: safe, hysteresis, danger
-        self.ks = 0.0  # Control blending factor
 
 
 
@@ -36,25 +33,23 @@ class PotentialFieldController(Node):
         self.user_cmd = msg
         self.compute_final_command()
 
-    def update_region_state(self, dist,danger_radius,hysteresis_radius):
-        rd = danger_radius    # Danger zone threshold
-        rh = hysteresis_radius   # Hysteresis (safe zone buffer)
+    def update_region_state(self, dist):
         prev = self.region_state
 
         if self.region_state == "safe":
-            if dist <= rd:
+            if dist <= self.rd:
                 self.region_state = "danger"
-            elif dist <= rh:
+            elif dist <= self.rh:
                 self.region_state = "hysteresis"
         elif self.region_state == "hysteresis":
-            if dist <= rd:
+            if dist <= self.rd:
                 self.region_state = "danger"
-            elif dist > rh:
+            elif dist > self.rh:
                 self.region_state = "safe"
         elif self.region_state == "danger":
-            if dist > rh:
+            if dist > self.rh:
                 self.region_state = "safe"
-            elif dist > rd:
+            elif dist > self.rd:
                 self.region_state = "hysteresis"
 
         # If region changed, store previous
@@ -62,18 +57,15 @@ class PotentialFieldController(Node):
             self.previous_region_state = prev
             self.get_logger().info(f"Region changed: {self.region_state} → {self.region_state}")
 
-    def compute_ks(self, dist,danger_radius,hysteresis_radius,epsilon,gamma ,beta):
-        rd = danger_radius    # Danger zone threshold
-        rh = hysteresis_radius   # Hysteresis (safe zone buffer)
-
-        self.update_region_state(dist,danger_radius,hysteresis_radius)
+    def compute_ks(self, dist):
+        self.update_region_state(dist)
         
         if self.region_state == "danger":
-            ks = np.tanh(beta * ((rd - dist) / rd) + epsilon)
+            ks = np.tanh(self.beta * ((self.rd - dist) / self.rd) + self.epsilon)
         elif self.region_state == "hysteresis":
             if self.previous_region_state == "danger":
                 # Entered from Md → use ks2
-                ks = np.tanh(epsilon) * np.tanh(gamma * ((rh - dist) / (rh - rd)))
+                ks = np.tanh(self.epsilon) * np.tanh(self.gamma * ((self.rh - dist) / (self.rh - self.rd)))
             else:
                 # Entered from Ms → reset to 0
                 ks = 0.0
@@ -83,13 +75,10 @@ class PotentialFieldController(Node):
         return ks
 
     def compute_final_command(self):
-        # ks is already computed
         final_cmd = Twist()
-        final_cmd.linear.x = (1 - self.ks) * self.user_cmd.linear.x + self.ks * self.potential_field_cmd.linear.x
-        final_cmd.angular.z = (1 - self.ks) * self.user_cmd.angular.z + self.ks * self.potential_field_cmd.angular.z
 
-        rd = 0.5    # Danger zone threshold
-        rh = 0.7    # Hysteresis (safe zone buffer)
+        lidar_min_val = 0
+        self.compute_ks(lidar_min_val)
 
         self.cmd_pub.publish(final_cmd)
 